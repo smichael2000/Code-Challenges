6fadae681642760d9504139e6808b3c9a7cdd791
Sheri Michael <smichael@tectonicgroup.com>
2018-06-25 20:28:13 -0600

    Working commit

--- question_mark_challenge.js
+++ question_mark_challenge.js
@@ -4,30 +4,26 @@
 // Returns true for every 3 ? between every number pair sums to 10
 // else the function returns false
 
-function QuestionMarks(str)
-{
-  ret = false;
-  //First number in pair
-  for(var x = 0; x < str.length; x++)
-  {
-    //Second number in pair
-    for(var y= x+1; y < str.length; y++)
-       {
-        if (parseInt(str[x]) + parseInt(str[y]) === 10)
-        {
-          if(str.slice(x+1,y).split('?').length - 1 === 3) {
-                ret = true;
-                break;
-            } // end of if(str.slice(x+1,y).split('?').length - 1 === 3
-            else {
-                return false;
-            } // end of else
-          } // end of if (parseInt(str[x]) + parseInt(str[y]) === 10)
-      } // end offor(var y= x+1; y < str.length; y++)
+function QuestionMarks(str) {
+  //Iterate for first number in pair
+  for(var x = 0; x < str.length; x++) {
+    //Iterate for second number in pair
+    for(var y = x+1; y < str.length; y++) {
+        if (Number(str[x]) + Number(str[y]) == 10) {
+          console.log((str[x]) + (str[y]) + " is 10");
+          //Create new array beginning str[x+1] & ending str[y]
+          //Array contains only ?
+          if (str.slice(x+1,y).split('?').length-1 >= 3) {
+            return true
+          } // end of if(str.slice(x+1,y).split('?').length >= 3
+          else {
+            return false
+          }
+        } // end of if (Number(str[x]) + Number(str[y]) === 10)
+    } // end of for(var y= x+1; y < str.length; y++)
   } // end of for(var x = 0; x < str.length; x++)
-  return ret;
 } // end of function
 
-// //test function QuestionMarks()
+//test function QuestionMarks()
 QuestionMarks("bcc?7??ccc?2tt1??????5");
 QuestionMarks("bb6?9");
